c496a926ef53a8fb7f06505b3b590f01b408a6e4
NaviHX <932942142@qq.com>
2020-10-16 19:50:33 +0800

    添加矩阵相乘

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
@@ -23,6 +24,8 @@ matrix *matrixInit(int row, int col, int elem[]);
 matrix *matrixPlus(matrix *a, matrix *b);
 matrix *matrixMultiply(matrix *a, matrix *b);
 void matrixPrint(matrix *m);
+matrix *matrixTrans(matrix *m);
+int *getRow(matrix *m, int row, node *p);
 
 void workPlus();
 void workMultiply();
@@ -149,6 +152,30 @@ matrix *matrixPlus(matrix *a, matrix *b)
     return c;
 }
 
+matrix *matrixTrans(matrix *m)
+{
+    matrix *ret = (matrix *)malloc(sizeof(matrix));
+    ret->row = m->col;
+    ret->col = m->row;
+    ret->size = m->size;
+    ret->elem = (node *)malloc(sizeof(node) * ret->col * ret->row);
+    int num[100] = {0}, cpot[100] = {-1};
+    for (int i = 0; i < m->size; i++)
+        num[m->elem[i].y]++;
+    cpot[0] = 0;
+    for (int i = 1; i < m->col; i++)
+        cpot[i] = cpot[i - 1] + num[i - 1];
+    for (int i = 0; i < m->size; i++)
+    {
+        int col = m->elem[i].y, row = m->elem[i].x;
+        ret->elem[cpot[col]].x = col;
+        ret->elem[cpot[col]].y = row;
+        ret->elem[cpot[col]].val = m->elem[i].val;
+        cpot[col]++;
+    }
+    return ret;
+}
+
 void workPlus()
 {
     int ar, ac;
@@ -182,7 +209,99 @@ void workPlus()
     }
 }
 
+int *getRow(matrix *m, int row, node *p)
+{
+    int *ret = (int *)malloc(sizeof(int) * m->col);
+    for (int i = 0; i < m->col; i++)
+        ret[i] = 0;
+    while (p->x == row)
+    {
+        ret[p->y] = p->val;
+        p++;
+    }
+    return ret;
+}
+
+matrix *matrixMultiply(matrix *a, matrix *b)
+{
+    if (a->col != b->row)
+        return NULL;
+    matrix *c = (matrix *)malloc(sizeof(matrix));
+    c->size = 0;
+    c->row = a->row;
+    c->col = b->col;
+    c->elem = (node *)malloc(sizeof(node) * c->col * c->row);
+    node *p = c->elem;
+    matrix *bTrans = matrixTrans(b);
+    swap(bTrans, b);
+    int apos[100];
+    int bpos[100];
+    memset(apos, -1, sizeof(apos));
+    memset(bpos, -1, sizeof(bpos));
+    for (int i = 0; i < a->size; i++)
+        if (apos[a->elem[i].x] == -1)
+            apos[a->elem[i].x] = i;
+    for (int i = 0; i < b->size; i++)
+        if (bpos[b->elem[i].x] == -1)
+            bpos[b->elem[i].x] = i;
+    for (int i = 0; i < a->row; i++)
+    {
+        if (apos[i] != -1)
+        {
+            for (int j = 0; j < b->row; j++)
+            {
+                int sum = 0;
+                if (bpos[j] != -1)
+                {
+                    int *va = getRow(a, i, a->elem + apos[i]);
+                    int *vb = getRow(b, j, b->elem + bpos[j]);
+                    for (int k = 0; k < a->col; k++)
+                        sum += va[k] * vb[k];
+                    if (sum)
+                    {
+                        p->val = sum;
+                        p->x = i;
+                        p->y = j;
+                        p++, c->size++;
+                    }
+                }
+            }
+        }
+    }
+    return c;
+}
+
 void workMultiply()
 {
+    int ar, ac;
+    int br, bc;
+    int *elema, *elemb;
+    cout << "请输入矩阵A行数和列数(e.g. 3 3):";
+    cin >> ar >> ac;
+    elema = (int *)malloc(sizeof(int) * ar * ac);
+    cout << "请输入矩阵A:\n";
+    for (int i = 0; i < ar; i++)
+        for (int j = 0; j < ac; j++)
+            cin >> elema[i * ac + j];
+    matrix *a = matrixInit(ar, ac, elema);
+    free(elema);
+    cout << "请输入矩阵B行数和列数(e.g. 3 3):";
+    cin >> br >> bc;
+    elemb = (int *)malloc(sizeof(int) * br * bc);
+    cout << "请输入矩阵B:\n";
+    for (int i = 0; i < br; i++)
+        for (int j = 0; j < bc; j++)
+            cin >> elemb[i * bc + j];
+    matrix *b = matrixInit(br, bc, elemb);
+    free(elemb);
+    //matrix* c=matrixTrans(b);
+    matrix *c = matrixMultiply(a, b);
+    if (c == NULL)
+        cout << "不可相乘: A列数不等于B行数\n";
+    else
+    {
+        cout << "乘积矩阵为:\n";
+        matrixPrint(c);
+    }
     return;
 }
